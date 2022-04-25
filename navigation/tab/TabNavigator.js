8bde7207aa5ee0965089c61f95eb4c01ad5d4ff9
Nahuel Valenzuela <nahu.vza@hotmail.com>
2022-04-25 18:15:48 -0300

    navigation terminado

--- navigation/tab/TabNavigator.js
+++ navigation/tab/TabNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet,View,Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import {Ionicons} from '@expo/vector-icons'
 
 import ShopNavigator from "../shop/ShopNavigator";
 import CartNavigator from "../cart";
@@ -10,14 +10,38 @@ const BottomTabs = createBottomTabNavigator ();
 
 const TabNavigator = () => (
     
-    <BottomTabs.Navigator initialRouteName="ShopNavigator">
+    <BottomTabs.Navigator initialRouteName="ShopNavigator"
+    screenOptions={{
+        headerShown: false,
+        tabBarShowLabel:false,
+        tabBarStyle: styles.tabBar
+    }}
+    >
      <BottomTabs.Screen
      name="Shop"
      component={ShopNavigator}
+     options={{
+         tabBarIcon:({focused})=>(
+             <View style={styles.item}>
+                 <Ionicons name="md-home" size={20} color="black" />
+                 <Text>Tienda</Text>
+             </View>
+         )
+     }}
+
      />
      <BottomTabs.Screen
      name="CartTab"
      component={CartNavigator}
+     options={{
+        tabBarIcon:({focused})=>(
+            <View style={styles.item}>
+                <Ionicons name="md-cart" size={20} color="black" />
+                <Text>Carrito</Text>
+            </View>
+        )
+    }}
+
      />
 
     </BottomTabs.Navigator>
@@ -25,6 +49,33 @@ const TabNavigator = () => (
 
 
     )
+
+    const styles = StyleSheet.create ({
+        tabBar:{
+            shadowColor:'#ccc',
+            shadowOffset:{width:0,height:10},
+            shadowOpacity: 0.25,
+            shadowRadius:0.25,
+            elevation:5,
+            position:'absolute',
+            bottom:25,
+            left: 20,
+            right:20,
+            borderRadius:15,
+            height:50,
+            backgroundColor:'#FFC300',
+            
+
+        },
+        item: {
+            flex:1,
+            justifyContent:'center',
+            alignItems:'center',
+
+        },
+
+
+    })
 
 export default TabNavigator
 
